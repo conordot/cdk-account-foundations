48bf0e5469134203d7c4ea07bac91ca6f4378e80
Conor Sinclair <conordotsinclair@gmail.com>
2021-06-12 19:03:46 +1000

    Add VPC stack

---
+++ cdk_enc/cdk_enc_vpc_stack.py
@@ -0,0 +1,12 @@
+from aws_cdk import core as cdk
+from aws_cdk import core
+
+
+
+class CdkEncVpcStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+        
---
+++ cdk_enc/lib/vpc.py
@@ -0,0 +1,18 @@
+import aws_cdk.ec2 as ec2
+
+class Vpc:
+    def __init__(self, *kwargs):
+        self.setup_vpc(kwargs.cidr.get("cidr", "10.0.0.0/16"))
+        self.subnets = []
+
+    def setup_vpc(self, *kwargs):
+        cidr = kwargs.get("cidr")
+        self.vpc = ec2.Vpc(self, kwargs.vpc_name,
+            cidr=cidr
+        )
+
+    def add_subnet(self, *kwargs):
+        az = kwargs.get("az", "ap-southeast-a")
+        cidr = kwargs.get("cidr", "10.0.0.0/16")
+        subnet = ec2.Subnet(self, kwargs.subnet_id, az, cidr, self.vpc.vpc_id)
+        self.subnets.append(subnet)
