56a9de155a3864142a494f79fc322473c7fc1c26
Conor Sinclair <conordotsinclair@gmail.com>
2021-06-13 21:12:11 +1000

    Add VPC stack

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ from aws_cdk import core as cdk
 from aws_cdk import core
 
 from cdk_enc.cdk_enc_stack import CdkEncStack
+from cdk_enc.cdk_enc_vpc_stack import CdkEncVpcStack
 
 
 app = core.App()
-CdkEncStack(app, "CdkEncStack",
+
+# VPC
+CdkEncStack(app, "CdkEncVpcStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -30,5 +33,23 @@ CdkEncStack(app, "CdkEncStack",
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
+
+# CdkEncStack(app, "CdkEncStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
+
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
+
+#     #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
+
+#     #env=core.Environment(account='123456789012', region='us-east-1'),
+
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#     )
 
 app.synth()
--- cdk_enc/cdk_enc_vpc_stack.py
+++ cdk_enc/cdk_enc_vpc_stack.py
@@ -1,12 +1,14 @@
 from aws_cdk import core as cdk
 from aws_cdk import core
-
+from lib.vpc import Vpc
 
 
 class CdkEncVpcStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
+        vpc = Vpc(cidr="10.8.0.0/16")
+        vpc.add_subnet(az="ap-southeast-2", cidr="10.8.0.0/24", subnet_id="public-a")
+        vpc.add_subnet(az="ap-southeast-2", cidr="10.9.0.0/24", subnet_id="private-a")
+        print(vpc.subnets)
         
